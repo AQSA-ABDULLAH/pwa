3f633f75640bde50b63488ae82c9865a5c3f09e8
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-05-29 20:23:16 +0500

    complete app

---
+++ app/head.js
@@ -0,0 +1,11 @@
+// app/head.js
+export default function Head() {
+  return (
+    <>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#000000" />
+      <link rel="icon" href="/icons/icon-192x192.png" />
+      <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+    </>
+  );
+}
