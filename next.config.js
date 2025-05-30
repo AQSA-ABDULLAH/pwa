508eb09600030197c4dc1d5e449e5c2851c735bd
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-05-30 11:27:04 +0500

    com

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
+
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
-  scope: '/app',
-  sw: 'service-worker.js',
+  // scope: '/app',
+  // sw: 'service-worker.js',
   //...
 })
 
 module.exports = withPWA({
   // next.js config
 })
+
